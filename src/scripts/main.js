4fc87edc3a5b034edc10cd8b82331ebbb0b10e59
Vanya Holovey <golovejvana9@gmail.com>
2025-07-01 21:14:58 +0300

    sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const ul = [...document.querySelectorAll('ul')];
+
+for (const list of ul) {
+  let title = list.previousSibling;
+
+  const span = document.createElement('span');
+  span.textContent = title.textContent.trim();
+  title.replaceWith(span);
+}
+
+const headers = [...document.querySelectorAll('span')];
+
+for (const header of headers) {
+  let visible = true;
+
+  header.addEventListener('click', () => {
+    if (visible) {
+      header.nextSibling.style.display = 'none';
+
+      visible = false;
+    } else {
+      header.nextSibling.style.display = '';
+      
+      visible = true;
+    }
+  });
+}
